57fb4317595efd12cd47f0e964becdb76983f784
rtoprila <51109205+rtoprila@users.noreply.github.com>
2019-06-16 15:58:14 +0300

    Add files via upload

---
+++ precache-manifest.eef2df2a5c285e2adaae7579c2e20026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4259f13d6120fb262d5",
+    "url": "/jdjajdjaid/static/js/runtime~main.e4259f13.js"
+  },
+  {
+    "revision": "99c2c69c1c58e6ddf5bb",
+    "url": "/jdjajdjaid/static/js/main.99c2c69c.chunk.js"
+  },
+  {
+    "revision": "5c2e9963341b683b0ab0",
+    "url": "/jdjajdjaid/static/js/2.5c2e9963.chunk.js"
+  },
+  {
+    "revision": "99c2c69c1c58e6ddf5bb",
+    "url": "/jdjajdjaid/static/css/main.e3a9f038.chunk.css"
+  },
+  {
+    "revision": "5c2e9963341b683b0ab0",
+    "url": "/jdjajdjaid/static/css/2.ddf38a60.chunk.css"
+  },
+  {
+    "revision": "209c4a1b39aa575315d462acc795240b",
+    "url": "/jdjajdjaid/index.html"
+  }
+];
